edd2a801b731af339e874de7f7a1df06848f4648
Ben Klopfenstein <benklop@gmail.com>
2008-04-02 18:49:40 +0000

    headers of new files updated to match everything else

--- nic.h
+++ nic.h
@@ -1,14 +1,26 @@
-//
-// C++ Interface: nic
-//
-// Description: provides a container for information about a particular NIC.
-//
-//
-// Author: Ben Klopfenstein <benklop@gmail.com>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/****************************************************************************
+**
+** Copyright (C) 2008 Ben Klopfenstein <benklop @ gmail.com>
+**
+** This file is part of QtEmu.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file COPYING.LIB.  If not, write to
+** the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA 02110-1301, USA.
+**
+****************************************************************************/
+
 #ifndef NIC_H
 #define NIC_H
 
